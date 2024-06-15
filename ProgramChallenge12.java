ef4939c29621edcf8dda704c2669f3287ceb7b4f
reneineedyou <141851157+reneineedyou@users.noreply.github.com>
2024-06-15 17:35:59 -0400

    Update ProgramChallenge12.java

--- ProgramChallenge12.java
+++ ProgramChallenge12.java
@@ -20,30 +20,36 @@ public class ProgramChallenge12 {
         // Create the scanner to get the user's input  
         Scanner scanner = new Scanner(System.in);
 
-        // Ask for user input
+        // Ask for user input weight
         System.out.print("Enter weight in kilograms: "); 
         double weight = scanner.nextDouble();
 
+        // Ask for user input weight
         System.out.print("Enter height in meters: "); 
         double height = scanner.nextDouble();
 
         //calculating Body Mass Index (BMI)
         double BMI = weight /(Math.pow(height,2));
 
+        //outputting the resulting statements
         System.out.print("\nBMI: " + String.format("%.2f", BMI) + "\t");
-
+        
         if (BMI < 18.5) {
             System.out.println("Underweight.");
-        } else if (BMI >= 18.5 && BMI < 24.9) {
+        } 
+        else if (BMI >= 18.5 && BMI < 24.9) {
             System.out.println("Normal.");
-        } else if (BMI >= 25 && BMI < 29.9) {
+        } 
+        else if (BMI >= 25 && BMI < 29.9) {
             System.out.println("Overweight.");
-        } else {
+        } 
+        else {
             System.out.println("Obese.");
-        
         }
+        
     // Close the scanner
     scanner.close();
 
     }
+    
 }
