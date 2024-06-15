d34b17bae9e161741ee4a079b7db33cdd07e38e8
reneineedyou <141851157+reneineedyou@users.noreply.github.com>
2024-06-15 14:37:02 -0400

    work

--- ProgramChallenge11.java
+++ ProgramChallenge11.java
@@ -8,10 +8,10 @@ Description: This program first prompts the user to enter a year as an int value
 A year is a leap year if it is divisible by 4 but not by 100, or it is divisible by 400.
 */
 import java.util.Scanner;
-import java.util.Random;
+
 
 public class ProgramChallenge11 {
-    public static void main(String[] args) {
+       public static void main(String[] args) {
 
         // Create the scanner to get the user's input  
         Scanner scanner = new Scanner(System.in);
@@ -20,10 +20,26 @@ public class ProgramChallenge11 {
         System.out.print("Enter a year and we will check if it's a leap year: "); 
         int year = scanner.nextInt();
 
-        if (year % 4 == 0 && year % 100 != 0 && year % 400 == 0)
+        // Checking for leap year
+        boolean leap = false;
+
+                if (year % 4 == 0) {
+            if (year % 100 != 0 || year % 400 == 0) {
+                leap = true;
+            }
+        }
+
+        // Output of the result
+        if (leap) {
             System.out.print( year + " is a leap year.");
-        else
-            System.out.print( year + " is not a leap year.");
-            
+        } 
+        else {
+            System.out.print( year + " is NOT a leap year.");
+        }            
+                
+        // Close the scanner
+        scanner.close();
     }
+
+
 }
---
+++ ProgramChallenge12.java
@@ -0,0 +1,49 @@
+/*
+Rene Gonzalez
+COP2800
+Programming Challenge 12
+
+Description: Body Mass Index (BMI) is a measure of health on weight. 
+It can be calculated by taking your weight in kilograms and dividing 
+by the square of your height in meters. The interpretation of BMI for 
+people 16 years or older is as follows:
+BMI < 18.5...Underweight
+18.5 >= BMI > 24.9...Normal
+25 >= BMI > 29.9...Overweight
+BMI > 30...Obese
+*/
+
+import java.util.Scanner;
+
+public class ProgramChallenge12 {
+    public static void main(String[] args) {
+        // Create the scanner to get the user's input  
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask for user input
+        System.out.print("Enter weight in kilograms: "); 
+        double weight = scanner.nextDouble();
+
+        System.out.print("Enter height in meters: "); 
+        double height = scanner.nextDouble();
+
+        //calculating Body Mass Index (BMI)
+        double BMI = weight /(Math.pow(height,2));
+
+        System.out.print("\nBMI: " + String.format("%.2f", BMI) + "\t");
+
+        if (BMI < 18.5) {
+            System.out.println("Underweight.");
+        } else if (BMI >= 18.5 && BMI < 24.9) {
+            System.out.println("Normal.");
+        } else if (BMI >= 25 && BMI < 29.9) {
+            System.out.println("Overweight.");
+        } else {
+            System.out.println("Obese.");
+        
+        }
+    // Close the scanner
+    scanner.close();
+
+    }
+}
