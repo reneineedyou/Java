ee704623892a227c431acc63caa2a01ef3a9ae6e
reneineedyou <141851157+reneineedyou@users.noreply.github.com>
2024-06-23 18:01:04 -0400

    practice midterm

---
+++ midterm.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class midterm {
+
+  public static void main(String[] args) {
+
+    
+
+    int a = 2;
+        int b = 8;
+        int c = a * b;
+        System.out.println(c);
+
+
+
+  }
+}
